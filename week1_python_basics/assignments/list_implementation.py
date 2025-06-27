0a89d0ae2d5f7a8431e04e0755df88e6803c5a4a
Jonnagiri Sheshadrinivas <jonnagirisheshadri12@gmail.com>
2025-06-27 16:03:12 +0530

    Create list_implementation.py

---
+++ week1_python_basics/assignments/list_implementation.py
@@ -0,0 +1,143 @@
+# Implement Stack using list, insert and delete from rear of the list
+
+#  import sys as s
+
+# arr = []
+# def push(num):
+#     arr.append(num)
+
+# def pop():
+#     arr.pop()
+
+# def prt():
+#     print(arr)
+
+# def invaild():
+#     print("invaild choice! ")
+#     sys.exit()
+
+# def stack(choice):
+#     if choice == 1:
+#         num = int(input("Enter value to push : "))
+    
+#     match(choice):
+#         case 1: push(num)
+#         case 2: pop()
+#         case 3: prt()
+#         case 4: s.exit()
+#         case _: invaild()
+
+# while True:
+#     print("1. push\n2. pop\n3. print\n4 exit")
+#     choice = int(input("Enter choice : "))
+#     stack(choice)
+
+
+# Implement Stack using list, insert and delete from front of the list
+
+# import sys as s
+
+# arr = []
+# def push(num):
+#     arr.insert(0, num)
+
+# def pop():
+#     del arr[0]
+
+# def prt():
+#     print(arr)
+
+# def invaild():
+#     print("invaild choice! ")
+#     sys.exit()
+
+# def stack(choice):
+#     if choice == 1:
+#         num = int(input("Enter value to push : "))
+    
+#     match(choice):
+#         case 1: push(num)
+#         case 2: pop()
+#         case 3: prt()
+#         case 4: s.exit()
+#         case _: invaild()
+
+# while True:
+#     print("1. push\n2. pop\n3. print\n4 exit")
+#     choice = int(input("Enter choice : "))
+#     stack(choice)
+
+
+# Implement Queue using list, insert at rear delete from front the list
+
+# import sys 
+
+# arr = []
+# def enqueue(num):
+#     arr.append(num)
+
+# def dequeue():
+#     del arr[0]
+
+# def print_queue():
+#     print(arr)
+
+# def invaild():
+#     print("invaild choice! ")
+#     sys.exit()
+
+# def queue(choice):
+#     if choice == 1:
+#         num = int(input("Enter value to push : "))
+    
+#     match(choice):
+#         case 1: enqueue(num)
+#         case 2: dequeue()
+#         case 3: print_queue()
+#         case 4: s.exit()
+#         case _: invaild()
+
+# while True:
+#     print("1. Enqueue\n2. Dequeue\n3. Print Queue\n4. Exit")
+#     choice = int(input("Enter choice : "))
+#     queue(choice)
+
+# Implement Queue using list, insert front, delete from rear of the list
+
+import sys 
+
+arr = []
+def enqueue(num):
+    arr.insert(0, num)
+
+def dequeue():
+    if len(arr) == 0:
+        print("Queue is empty!")
+        break
+    arr.pop()
+
+def print_queue():
+    if len(arr) == 0:
+        print("Queue is empty!")
+        break
+    print(arr)
+
+def invaild():
+    print("invaild choice! ")
+    sys.exit()
+
+def queue(choice):
+    if choice == 1:
+        num = int(input("Enter value to push : "))
+    
+    match(choice):
+        case 1: enqueue(num)
+        case 2: dequeue()
+        case 3: print_queue()
+        case 4: sys.exit()
+        case _: invaild()
+
+while True:
+    print("1. Enqueue\n2. Dequeue\n3. Print Queue\n4. Exit")
+    choice = int(input("Enter choice : "))
+    queue(choice)
