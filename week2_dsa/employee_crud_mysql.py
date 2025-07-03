9239ee3843c7b43a8dbdb69c7e82fd000c590aa9
jonnagiri12 <jonnagirisheshadri12@gmail.com>
2025-07-03 12:08:07 +0530

    Db

---
+++ week2_dsa/employee_crud_mysql.py
@@ -0,0 +1,79 @@
+import pymysql
+
+def connect_db():
+    connection = None
+    try:
+        connection = pymysql.connect(host='localhost', user="root", password="Bobby@1211", database='sheshadri_db', port=3306, charset="utf8")
+        print('Database Connected')
+    except:
+        print('Database Connection Failed')
+    return connection
+
+def disconnect_db(connection):
+    try:
+        connection.close()
+        print('Database disconnected')
+    except:
+        print('Database disconnection failed')
+
+def creat_db():
+    query = 'create database IF NOT EXISTS sheshadri_db'
+    connection = connect_db()
+    try:
+        cursor = connection.cursor()
+        cursor.execute(query)
+        print('Database created')
+        cursor.close()
+        disconnect_db(connection)
+    except:
+        print('Database creation failed')
+
+
+def create_table():
+    query = 'create table IF NOT EXISTS employees(id int primary key auto_increment,
+name			varchar(30)	not null,
+designation		varchar(30)	,
+phone_number	bigint		unique,
+salary			float		,
+commission		float		default(0),
+years_of_experience tinyint	,
+technology		varchar(30)	not null,
+)'
+    connection = connect_db()
+    try:
+        cursor = connection.cursor()
+        cursor.execute(query)
+        print('Table created')
+        cursor.close()
+        disconnect_db(connection)
+    except:
+        print('Table creation failed')
+
+def read_all_employees():
+    query = 'select * from employees'
+    connection = connect_db()
+    try:
+        cursor = connection.cursor()
+        cursor.execute(query)
+        rows = cursor.fetchAll()
+        for row in rows:
+            print(row)
+        print('All rows retrived')
+        
+        cursor.close()
+        disconnect_db(connection)
+    except:
+        print('Rows retrival failed')
+
+
+# connection = connect_db()
+
+# connection.close() # to disconnect the DB
+
+# disconnect_db(connection)
+
+# creat_db()
+
+create_table()
+
+# read_all_employees()
